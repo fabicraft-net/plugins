0685dc4d05b6dcc7bcb98a036f0f32392e9d68f5
Fabian Mäkilä <contact@fabianadrian.fi>
2026-07-29 20:05:15 +0300

    remove unused imports

--- paper-core/src/main/java/net/fabicraft/paper/core/locale/CoreTranslationManager.java
+++ paper-core/src/main/java/net/fabicraft/paper/core/locale/CoreTranslationManager.java
@@ -3,8 +3,6 @@ package net.fabicraft.paper.core.locale;
 import net.kyori.adventure.key.Key;
 import org.slf4j.Logger;
 
-import java.util.List;
-import java.util.Locale;
 
 public final class CoreTranslationManager extends net.fabicraft.common.locale.TranslationManager {
 	public CoreTranslationManager(Logger logger) {
