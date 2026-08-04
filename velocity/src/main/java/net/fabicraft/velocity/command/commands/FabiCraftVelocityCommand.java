681255006aa3de8abfbeaacbeb0b0a0dc2361025
Fabian Mäkilä <contact@fabianadrian.fi>
2026-08-04 16:21:04 +0300

    implement reload functionality

--- velocity/src/main/java/net/fabicraft/velocity/command/commands/FabiCraftVelocityCommand.java
+++ velocity/src/main/java/net/fabicraft/velocity/command/commands/FabiCraftVelocityCommand.java
@@ -1,15 +1,24 @@
 package net.fabicraft.velocity.command.commands;
 
+import com.velocitypowered.api.command.CommandSource;
 import net.fabicraft.velocity.FabiCraftVelocity;
 import net.fabicraft.velocity.command.VelocityCommand;
+import org.incendo.cloud.context.CommandContext;
 
 public final class FabiCraftVelocityCommand extends VelocityCommand {
+	private static final String PERMISSION_RELOAD = "fabicraft.velocity.command.fabicraftvelocity.reload";
+
 	public FabiCraftVelocityCommand(FabiCraftVelocity plugin) {
 		super(plugin);
 	}
 
 	@Override
 	public void register() {
+		var builder = super.manager.commandBuilder("fabicraftvelocity");
+		super.manager.command(builder.literal("reload").permission(PERMISSION_RELOAD).handler(this::handleReload));
+	}
 
+	private void handleReload(CommandContext<CommandSource> context) {
+		super.plugin.load();
 	}
 }
